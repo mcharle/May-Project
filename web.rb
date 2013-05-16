e740801ba60f56c08e360d1b95ddac37d01b95ed
Meryl Charleston <meryl.charleston@gmail.com>
2013-05-16 11:59:57 -0500

    create Post and Tag classes, add post_new and post/create, add database

--- web.rb
+++ web.rb
@@ -1,8 +1,32 @@
-require 'sinatra'
-require 'sass'
+require 'bundler'
+
+Bundler.require
+
+DataMapper.setup(:default, 'sqlite:///Users/merylcharleston/Documents/mayproject/db/mp.db')
+
+class Post
+  include DataMapper::Resource
+
+  property :id,       Serial
+  property :title,    String
+  property :date,     Date
+  property :content,  Text
+
+  has n, :tags
+
+end
+
+class Tag
+  include DataMapper::Resource
+
+  property :id,       Serial
+  property :category, String
+end
+
+DataMapper.finalize
+DataMapper.auto_upgrade!
 
 use Rack::Static, :urls => ['/images'], :root => 'public'
-
 
 get '/style.css' do
 	scss :style
@@ -12,6 +36,18 @@ get '/' do
 	haml :index
 end
 
+get '/post/new' do
+  haml :post_new
+end
+
+get '/post/create' do
+  post = Post.new(title: params[:title])
+end
+
 get '/about' do
 	haml :about
+end
+
+get '/faq' do
+  haml :faq
 end
